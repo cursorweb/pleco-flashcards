e6e078a25e0cf522ad052cc3c10adbb5fb98d09c
Junhao Zhang <jerry_mountain@hotmail.com>
2023-10-07 15:18:42 -0500

    updated code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use eframe::egui;
+use eframe::egui::{self, FontDefinitions};
 
 fn main() {
     let native_options = eframe::NativeOptions::default();
+
     eframe::run_native(
         "Pleco Card Review",
         native_options,
@@ -11,14 +12,21 @@ fn main() {
 }
 
 #[derive(Default)]
-struct MyEguiApp;
+struct MyEguiApp {
+    counter: i32,
+    my_string: String,
+    my_f32: f32,
+}
 
 impl MyEguiApp {
-    fn new(_: &eframe::CreationContext<'_>) -> Self {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
         // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
         // Restore app state using cc.storage (requires the "persistence" feature).
         // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
         // for e.g. egui::PaintCallback.
+        let font_defs = FontDefinitions::default();
+        cc.egui_ctx.set_fonts(font_defs);
+        cc.egui_ctx.set_pixels_per_point(2f32);
         Self::default()
     }
 }
@@ -26,7 +34,19 @@ impl MyEguiApp {
 impl eframe::App for MyEguiApp {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Hello World!");
+            ui.vertical(|ui| {
+                ui.hyperlink("https://github.com/emilk/egui");
+
+                ui.label(format!("Count: {}", self.counter));
+
+                if ui.button("+1").clicked() {
+                    self.counter += 1;
+                }
+
+                if ui.button("-1").clicked() {
+                    self.counter -= 1;
+                }
+            });
         });
     }
 }

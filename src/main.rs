4f77018d0a5cc89effde3ad8441a45a78258bf5a
Junhao Jerry Zhang <jerry_mountain@hotmail.com>
2023-10-10 23:12:48 -0500

    todo - manually calculate
    using the vertically centered etc is TOO hard

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use eframe::{
-    egui::{self, Align, FontData, FontDefinitions, Layout, RichText, Ui},
+    egui::{self, Align, Color32, FontData, FontDefinitions, Layout, RichText, Ui},
     epaint::{FontFamily, Vec2},
 };
 use font_kit::{
@@ -8,13 +8,14 @@ use font_kit::{
 use pleco_study::{Card, Reviewer};
 
 const TITLE_SIZE: f32 = 64.0;
+const ROUNDING: f32 = 5.0;
 const WIDTH: f32 = 400.0;
 const HEIGHT: f32 = 400.0;
 
 fn main() {
     let mut native_options = eframe::NativeOptions::default();
     native_options.initial_window_size = Some(Vec2::new(WIDTH, HEIGHT));
-    native_options.resizable = false;
+    // native_options.resizable = false;
 
     eframe::run_native(
         "Pleco Card Review",
@@ -37,12 +38,7 @@ struct MyEguiApp {
 }
 
 impl MyEguiApp {
-    fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
-        // Restore app state using cc.storage (requires the "persistence" feature).
-        // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
-        // for e.g. egui::PaintCallback.
-
+    fn load_fonts(cc: &eframe::CreationContext<'_>) {
         let source = SystemSource::new();
         let font =
             source.select_best_match(&[FamilyName::Title("kaiti".into())], &Properties::default());
@@ -68,6 +64,18 @@ impl MyEguiApp {
             .push("chinese".into());
 
         cc.egui_ctx.set_fonts(fonts);
+    }
+
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
+        // Restore app state using cc.storage (requires the "persistence" feature).
+        // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
+        // for e.g. egui::PaintCallback.
+        Self::load_fonts(cc);
+
+        cc.egui_ctx.style_mut(|style| {
+            style.visuals.override_text_color = Some(Color32::BLACK);
+        });
 
         let mut reviewer = Reviewer::load_cards("flash.txt");
         let (strength, card) = reviewer.next_card().expect("Should not be empty");
@@ -88,7 +96,8 @@ impl MyEguiApp {
             }
 
             egui::Frame::none()
-                .fill(egui::Color32::from_rgb(123, 123, 233))
+                .fill(Color32::from_rgb(123, 123, 233))
+                .rounding(ROUNDING)
                 .show(ui, |ui| {
                     ui.centered_and_justified(|ui| {
                         ui.label(RichText::new(&self.card.simp).size(TITLE_SIZE));
@@ -98,31 +107,41 @@ impl MyEguiApp {
     }
 
     fn render_back(&mut self, ui: &mut Ui) {
-        ui.label(&self.card.pinyin);
-        ui.label(
-            RichText::new(format!(
-                "{}{}",
-                self.card.simp,
-                if self.card.simp != self.card.trad {
-                    format!("[{}]", self.card.trad)
-                } else {
-                    "".into()
-                }
-            ))
-            .size(TITLE_SIZE),
-        );
-        ui.label(&self.card.def);
+        ui.with_layout(Layout::bottom_up(Align::Center), |ui| {
+            if ui.button("Next").clicked() {
+                let (next_strength, mut next_card) = self.reviewer.next_card().expect("All done!");
 
-        if ui.button("Next").clicked() {
-            let (next_strength, mut next_card) = self.reviewer.next_card().expect("All done!");
+                std::mem::swap(&mut self.card, &mut next_card);
+                let old_card = next_card;
+                self.next_reviewer.studied_card(old_card, self.strength);
+                self.strength = next_strength;
 
-            std::mem::swap(&mut self.card, &mut next_card);
-            let old_card = next_card;
-            self.next_reviewer.studied_card(old_card, self.strength);
-            self.strength = next_strength;
+                self.front_side = true;
+            }
 
-            self.front_side = true;
-        }
+            egui::Frame::none()
+                .fill(Color32::from_rgb(6, 209, 20))
+                .rounding(ROUNDING)
+                .show(ui, |ui| {
+                    ui.vertical_centered_justified(|ui| {
+                        ui.label(&self.card.pinyin);
+                        ui.label(
+                            RichText::new(format!(
+                                "{}{}",
+                                self.card.simp,
+                                if self.card.simp != self.card.trad {
+                                    format!("[{}]", self.card.trad)
+                                } else {
+                                    "".into()
+                                }
+                            ))
+                            .size(TITLE_SIZE),
+                        );
+                        ui.label(&self.card.def);
+                    });
+                });
+            // });
+        });
     }
 }
 
